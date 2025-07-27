a8fe46c3d737a1426238b6bdcd91fd871dc811d9
Igor Bessonov <ikogep23@gmail.com>
2025-07-27 10:15:40 +0300

    add new templates with inheritance

--- hexlet_django_blog/hexlet_django_blog/article/views.py
+++ hexlet_django_blog/hexlet_django_blog/article/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+#def index(request):
+#    return HttpResponse("<h1>article</h1>")
+
+
 def index(request):
-    return HttpResponse("article")
+    return render(
+        request,
+        "index.html",
+        context={
+            'who': 'Article',
+        }
+    )
 
 # Create your views here.
