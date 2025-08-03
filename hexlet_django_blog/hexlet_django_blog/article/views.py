0897de5f6fe957a42ad4cd4a4a75ae589f01b81b
Igor Bessonov <ikogep23@gmail.com>
2025-08-03 09:43:42 +0300

    upd views to class views with inheritance

--- hexlet_django_blog/hexlet_django_blog/article/views.py
+++ hexlet_django_blog/hexlet_django_blog/article/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from hexlet_django_blog.views import IndexView
 
 
 #def index(request):
 #    return HttpResponse("<h1>article</h1>")
 
 
-def index(request):
-    return render(
-        request,
-        "index.html",
-        context={
-            'who': 'Article',
-        }
-    )
+#def index(request):
+#    return render(
+#        request,
+#        "index.html",
+#        context={
+#            'who': 'Article',
+#        }
+#    )
+
+
+class ArticleView(IndexView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["who"] = "Article"
+        return context
 
 # Create your views here.
--- hexlet_django_blog/hexlet_django_blog/views.py
+++ hexlet_django_blog/hexlet_django_blog/views.py
@@ -1,15 +1,17 @@
 # hexlet_django_blog/views.py
+# from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from django.views.generic.base import TemplateView
 
 
-def index(request):
-    return render(
-        request,
-        "index.html",
-        context={
-            'who': 'World',
-        }
-    )
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["who"] = "World"
+        return context
 
 
 def about(request):
